d618cf6c568898c3b7c59bb8bd933cc8280755ac
shevchenko-ivan <shevchenkoivan1951@gmail.com>
2022-05-28 17:21:21 +0200

    test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import './style.css'
 
 const Header = () =>{
   return <>
-    <div className="bg-video">
-      sdvdv
+    <div className="top">
+      <img src="../../assets/img/Ivan_Shevchenko.svg" alt="logo" />
     </div>
 
   </>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './components/App'
+import App from './components/App/index'
 import reportWebVitals from './reportWebVitals'
 
-// import './index.css'
-// import './assets/style/colors.css'
-// import './assets/style/fonts.css'
-// import './assets/style/bootstrap-grid.css'
-// import 'react-phone-number-input/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
